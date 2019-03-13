2f68041435b0992eacc93aa0c568305df302f074
David Albert <davidbalbert@gmail.com>
2019-03-13 16:47:05 -0400

    Initial Commit

---
+++ Say It/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Say It
+//
+//  Created by David Albert on 3/13/19.
+//  Copyright © 2019 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Say It/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Say It
+//
+//  Created by David Albert on 3/13/19.
+//  Copyright © 2019 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
