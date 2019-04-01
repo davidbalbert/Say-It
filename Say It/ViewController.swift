0d943ad98333ac0ed775aa9390fef0941b48a49d
David Albert <davidbalbert@gmail.com>
2019-04-01 14:55:17 -0400

    Swift 5 + some UI

--- Say It/ViewController.swift
+++ Say It/ViewController.swift
@@ -8,12 +8,23 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSSpeechSynthesizerDelegate {
+
+    @IBOutlet var voicesMenu: NSPopUpButton!
+    @IBOutlet var textField: NSTextField!
+    @IBOutlet var sayItButton: NSButton!
+
+    func voiceNames() -> [String] {
+        return NSSpeechSynthesizer.availableVoices.map { voice in
+            NSSpeechSynthesizer.attributes(forVoice: voice)[.name] as! String
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        voicesMenu.removeAllItems()
+        voicesMenu.addItems(withTitles: voiceNames())
     }
 
     override var representedObject: Any? {
@@ -22,6 +33,16 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func sayIt(_ sender: NSButton) {
+        let synth = NSSpeechSynthesizer()
+        synth.delegate = self
+        sayItButton.isEnabled = false
 
+        synth.startSpeaking(textField.stringValue)
+    }
+
+    func speechSynthesizer(_ sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
+        sayItButton.isEnabled = true
+    }
 }
 
