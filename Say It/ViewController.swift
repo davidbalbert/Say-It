cc1e522f9bed00bd729643ca57554ebaa1c48eaf
David Albert <davidbalbert@gmail.com>
2019-04-02 17:09:10 -0400

    Lots of improvements, too lazy to enumerate

--- Say It/ViewController.swift
+++ Say It/ViewController.swift
@@ -13,6 +13,52 @@ class ViewController: NSViewController, NSSpeechSynthesizerDelegate {
     @IBOutlet var voicesMenu: NSPopUpButton!
     @IBOutlet var textField: NSTextField!
     @IBOutlet var sayItButton: NSButton!
+    @IBOutlet var sayItFromClipboardButton: NSButton!
+    @IBOutlet var stopButton: NSButton!
+
+    @IBOutlet var rateSlider: NSSlider!
+    @IBOutlet var rateTextField: NSTextField!
+
+    let synth = NSSpeechSynthesizer()
+
+    let minRate: Int32 = 100
+    let maxRate: Int32 = 800
+
+    var speaking = false {
+        didSet {
+            if speaking {
+                textField.isEnabled = false
+                sayItButton.isEnabled = false
+                sayItFromClipboardButton.isEnabled = false
+                rateSlider.isEnabled = false
+                rateTextField.isEnabled = false
+
+                stopButton.isEnabled = true
+            } else {
+                textField.isEnabled = true
+                sayItButton.isEnabled = true
+                sayItFromClipboardButton.isEnabled = true
+                rateSlider.isEnabled = true
+                rateTextField.isEnabled = true
+
+                stopButton.isEnabled = false
+            }
+        }
+    }
+
+    var rate: Int32! {
+        didSet {
+            if rate < minRate {
+                rate = minRate
+            } else if rate > maxRate {
+                rate = maxRate
+            }
+
+            rateSlider.intValue = rate
+            rateTextField.intValue = rate
+            synth.rate = Float(rate)
+        }
+    }
 
     func voiceNames() -> [String] {
         return NSSpeechSynthesizer.availableVoices.map { voice in
@@ -25,6 +71,12 @@ class ViewController: NSViewController, NSSpeechSynthesizerDelegate {
 
         voicesMenu.removeAllItems()
         voicesMenu.addItems(withTitles: voiceNames())
+
+        synth.delegate = self
+
+        rate = Int32(synth.rate)
+        rateSlider.minValue = Double(minRate)
+        rateSlider.maxValue = Double(maxRate)
     }
 
     override var representedObject: Any? {
@@ -34,15 +86,35 @@ class ViewController: NSViewController, NSSpeechSynthesizerDelegate {
     }
 
     @IBAction func sayIt(_ sender: NSButton) {
-        let synth = NSSpeechSynthesizer()
-        synth.delegate = self
-        sayItButton.isEnabled = false
-
+        speaking = true
         synth.startSpeaking(textField.stringValue)
     }
 
+    @IBAction func sayItFromClipboard(_ sender: NSButton) {
+        guard let text = NSPasteboard.general.string(forType: .string) else {
+            let alert = NSAlert()
+            alert.addButton(withTitle: "OK")
+            alert.messageText = "No text in the clipboard"
+            alert.informativeText = "There's no text in the clipboard, so there's nothing to say. Copy some text and try again."
+            alert.alertStyle = .warning
+            alert.beginSheetModal(for: view.window!, completionHandler: nil)
+            return
+        }
+
+        speaking = true
+        synth.startSpeaking(text)
+    }
+
     func speechSynthesizer(_ sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
-        sayItButton.isEnabled = true
+        speaking = false
+    }
+
+    @IBAction func updateRate(_ sender: NSControl) {
+        rate = sender.intValue
+    }
+
+    @IBAction func stopSpeaking(_ sender: Any) {
+        synth.stopSpeaking()
     }
 }
 
