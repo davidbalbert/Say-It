503e2fc5c497fae3b996edd4ab084d898a513b72
David Albert <davidbalbert@gmail.com>
2020-03-02 09:56:44 -0500

    Set title of preferences window from tab view

---
+++ Say It/PreferencesTabViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PreferencesController.swift
+//  Say It
+//
+//  Created by David Albert on 3/2/20.
+//  Copyright © 2020 David Albert. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesTabViewController: NSTabViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+
+    override func tabView(_ tabView: NSTabView, didSelect tabViewItem: NSTabViewItem?) {
+        super.tabView(tabView, didSelect: tabViewItem)
+
+        setWindowTitle()
+    }
+
+    func setWindowTitle() {
+        view.window?.title = title ?? "Preferences"
+    }
+}
