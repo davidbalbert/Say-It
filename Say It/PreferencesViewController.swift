263c6cdf3ec5585a6e1b9cca8a7cd3ddc1eedabb
David Albert <davidbalbert@gmail.com>
2020-03-18 21:49:26 -0400

    Window resizing that makes sense :)

--- Say It/PreferencesViewController.swift
+++ Say It/PreferencesViewController.swift
@@ -9,18 +9,47 @@
 import Cocoa
 
 class PreferencesViewController: NSTabViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
+    override func viewWillAppear() {
+        guard let view = tabView.selectedTabViewItem?.view else {
+            return
+        }
+
+        sizeWindowToContent(of: view, animate: false)
+    }
+
+    override func tabView(_ tabView: NSTabView, willSelect tabViewItem: NSTabViewItem?) {
+        super.tabView(tabView, willSelect: tabViewItem)
+
+        guard let oldView = tabView.selectedTabViewItem?.view, let newView = tabViewItem?.view  else {
+            return
+        }
+
+        oldView.isHidden = true
+        newView.isHidden = true
+
+        sizeWindowToContent(of: newView, animate: true)
     }
 
     override func tabView(_ tabView: NSTabView, didSelect tabViewItem: NSTabViewItem?) {
         super.tabView(tabView, didSelect: tabViewItem)
 
-        setWindowTitle()
+        tabViewItem?.view?.isHidden = false
+        view.window?.title = title ?? "Preferences"
     }
 
-    func setWindowTitle() {
-        view.window?.title = title ?? "Preferences"
+    func sizeWindowToContent(of newView: NSView, animate: Bool) {
+        guard let window = view.window else {
+            return
+        }
+
+        let contentSize = newView.fittingSize
+        let newWindowSize = window.frameRect(forContentRect: NSRect(origin: NSPoint.zero, size: contentSize)).size
+
+        var frame = window.frame
+        frame.origin.y += frame.size.height
+        frame.origin.y -= newWindowSize.height
+        frame.size = newWindowSize
+
+        window.setFrame(frame, display: false, animate: animate)
     }
 }
